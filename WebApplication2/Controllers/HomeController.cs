f016420140e14aaac7448c24315ca68e781f450a
DeVice233 <yakovenko182003@gmail.com>
2021-10-20 16:34:42 +1000

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        [Authorize(Roles = "admin, user")]
+        public async Task<IActionResult> Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            //return Content($"ваша роль: {role}");
+            ViewData["role"] = $"Ваша роль: {role}";
+            return View(await db.Users.ToListAsync()); 
+        }
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Users()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+        [HttpPost, Authorize(Roles = "admin")]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost, Authorize(Roles = "admin")]
+        public async Task<IActionResult> Edit(User user)
+        {
+            db.Users.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete"), Authorize(Roles = "admin")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost, Authorize(Roles = "admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User user = new User { Id = id.Value };
+                db.Entry(user).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+        
+        [Authorize(Roles = "admin")]
+        public IActionResult Delete()
+        {
+            return Content("Вход только для администратора");
+        }
+     
+
+    }
+}
---
+++ WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('.toast').toast(option);
+function toggleShowPassword() {
+	var passwordTextBox = document.getElementById('passwordTextBox');
+	if (passwordTextBox.getAttribute('type') == 'text') {
+		passwordTextBox.type = 'password';
+	}
+	else {
+		passwordTextBox.type = 'text';
+	}
+}
